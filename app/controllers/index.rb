a2971e07b09173d286e88bc39bc4c9fade27651f
Test <nextacademy@email.com>
2015-10-21 10:33:12 +0800

    setting up start page, and defined authen in user

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,3 @@
 get '/' do
-  @aunty = params[:aunty]
-  puts @aunty
-  # Look in app/views/index.erb
   erb :index
-end
-
-post '/aunty' do
-  speech = params[:user_input]
-  redirect to("/?aunty=#{speech}")
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
+
+  def self.authen(username, password)
+  	#if username and password match redirect to next page
+  	#else return nil
+  end
 end
