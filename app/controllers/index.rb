f7912433eb775fc0aa86c69e18d43ca5d7d5b7cb
Test <nextacademy@email.com>
2015-10-21 11:21:00 +0800

    login system works

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,16 @@
+require 'byebug'
+enable :session
+
 get '/' do
   erb :index
 end
 
 get '/sign_up' do
   erb :sign_up
+end
+
+get '/home_page' do
+  erb :home_page
 end
 
 post '/create_user' do
@@ -13,5 +20,11 @@ post '/create_user' do
 end
 
 post '/login_user' do
+  @checker = User.authen(params[:username], params[:password])
 
+  if @checker == true
+	redirect '/home_page'
+  else
+	redirect '/'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
 
-  def self.authen(username, password)
+  def self.authen(name, pass)
   	#if username and password match redirect to next page
   	#else return nil
+  	if User.find_by(username: name, password: pass)
+  		return true
+  	else
+  		return nil
+  	end
   end
 end
